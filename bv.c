c358238d8896cf07167a1e3c88009976e917512d
AHapper <aneka09@gmail.com>
2018-06-07 15:12:29 -0400

    bv.c

---
+++ bv.c
@@ -0,0 +1,50 @@
+// assign1
+// bv.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "bv.h"
+
+bitV *newVec(uint32_t len){
+    int len_b = len/8;
+    bitV *myvector = malloc(sizeof(bitV) + len_b * sizeof(uint8_t));
+    int sum = sizeof(bitV) + len_b * sizeof(uint8_t);
+    printf("the sum is %d\n", sum);
+    myvector->length = len;
+    for (int i = 0; i < len_b; i++){
+    //    myvector->vector[i] = 0;
+    };
+    return myvector;
+};
+
+void printVector(bitV *myvector)
+{
+    printf("length : %d\n",myvector->length);
+    printf("vector : %s\n",myvector->vector);
+}
+ 
+
+/* void delVec(bitV *){
+
+};
+
+void oneVec(bitV *){
+
+};
+
+void setBit(bitV *, uint32_t){
+
+};
+
+void clrBit(bitV *, uint32_t){
+
+};
+
+uint8_t valBit(bitV *, uint32_t){
+    return "0";
+};
+
+uint32_t lenVec(bitV *){
+    return 0;
+};
+
+*/
