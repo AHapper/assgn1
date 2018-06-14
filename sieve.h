86ceef59dceab84a86e206c9e3791cefa944df1b
AHapper <aneka09@gmail.com>
2018-06-14 14:56:12 -0400

    Create sieve.h

---
+++ sieve.h
@@ -0,0 +1,12 @@
+// Aneka Happer
+// June 5, 2018
+// sieve.h
+// from assign1, credited to D. Long
+
+# ifndef _SIEVE
+# define _SIEVE
+# include "bv.h"
+
+void sieve(bitV *); // Results in a vector of prime numbers
+
+# endif
